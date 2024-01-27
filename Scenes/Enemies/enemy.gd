4161c85649acfbe6cd19ec779392b71a869912b3
Ryan Doyle (RIT Student) <rad8555@rit.edu>
2024-01-27 12:11:11 -0500

    Stapling fixes

--- Scenes/Enemies/enemy.gd
+++ Scenes/Enemies/enemy.gd
@@ -6,11 +6,13 @@ class_name Enemy
 @export var Speed : float;
 @export var HP : int;
 @export var StapleSpeed : float;
+@export var StapleHeightOffset : float;
 
 # Object Vars
 var move_dir : Vector3 = Vector3.ZERO;
 var stapled : bool = false;
 var stuck : bool = false;
+var gravity : float = ProjectSettings.get_setting("physics/3d/default_gravity");
 
 
 # Component Vars
@@ -20,6 +22,9 @@ var stuck : bool = false;
 
 
 # Processes
+
+func _ready() -> void:
+	pass
 
 func _physics_process(delta: float) -> void:
 	if(stuck):
@@ -32,17 +37,22 @@ func _physics_process(delta: float) -> void:
 	move_and_slide();
 	
 	var collision : KinematicCollision3D = get_last_slide_collision();
-	if(collision != null):
+	if(collision != null and stapled):
 		if(collision.get_collider().is_in_group("Enemies")):
 			return;
+		if(collision.get_collider().is_in_group("Staples")):
+			return;
+		
 		stuck = true;
 		BodySprite.billboard = BaseMaterial3D.BILLBOARD_DISABLED;
-		BodySprite.look_at(collision.get_normal());
+		BodySprite.look_at(collision.get_normal() * 500);
+		
 
 func _on_Stapled(staple_pos : Vector3) -> void:
-	move_dir = (staple_pos - global_position).normalized();
+	move_dir = ((staple_pos - Vector3(0, StapleHeightOffset, 0)) - global_position).normalized();
 	move_dir = move_dir * -1;
-	BodySprite.billboard = BaseMaterial3D.BILLBOARD_ENABLED;
+	BodySprite.billboard = BaseMaterial3D.BILLBOARD_DISABLED;
+	BodySprite.look_at(move_dir);
 	stapled = true;
 	RegularCollider.set_deferred("disabled", true);
 	StapledCollider.set_deferred("disabled", false);
@@ -51,7 +61,9 @@ func _on_Stapled(staple_pos : Vector3) -> void:
 # Functions
 
 func moveNormal() -> void:
-	pass;
+	velocity = Vector3.ZERO;
+	if(!is_on_floor()):
+		velocity = Vector3(0, gravity * -1, 0);
 
 func moveStapled() -> void:
 	velocity = move_dir * StapleSpeed;
