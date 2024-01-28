340f8a16911dbf1de41b2f1403cadc2e6ed5ec82
Ryan Doyle (RIT Student) <rad8555@rit.edu>
2024-01-28 14:54:57 -0500

    asinhtkjasfh

--- Scenes/UI/Cutscene.gd
+++ Scenes/UI/Cutscene.gd
@@ -8,3 +8,7 @@ func _on_CutsceneBegin() -> void:
 
 func endCutscene() -> void:
 	get_tree().call_group("CBegin", "_on_Begin");
+
+
+func _on_finished():
+	get_tree().call_group("CBegin", "_on_Begin");
