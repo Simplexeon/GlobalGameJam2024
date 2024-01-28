af7129db9e3e6b9efac743e4d96fb021572c8b79
Simplexeon <gamerguy941mc@gmail.com>
2024-01-28 11:25:57 -0500

    Main menu stuff

---
+++ Scenes/Menus/MainMenu/CreditsButton.gd
@@ -0,0 +1,23 @@
+@tool
+extends TextureButton
+
+# Export Vars
+@export var CenterOffset : Vector2;
+
+
+# Processes
+
+func _process(delta: float) -> void:
+	if(Engine.is_editor_hint() == false):
+		return;
+	anchors_preset = PRESET_CENTER;
+	anchor_bottom = 0.5;
+	anchor_top = 0.5;
+	anchor_left = 0.5;
+	anchor_right = 0.5;
+	position += CenterOffset;
+
+
+func _on_pressed() -> void:
+	get_tree().call_group("COpenCredits", "_on_OpenCredits");
+
--- Scenes/Menus/MainMenu/real_menu.gd
+++ Scenes/Menus/MainMenu/real_menu.gd
@@ -1,10 +1,14 @@
 extends Node2D
 
+# Component vars
+@onready var Credits : Control = $Credits;
+
 #Files
 @onready var trans_file = preload("res://Scenes/Menus/ScreenTransition.tscn");
 
 
 func _ready() -> void:
+	Credits.visible = false;
 	var Transition : TransitionNode = trans_file.instantiate();
 	add_child(Transition);
 	Transition.call_deferred("initialize", Vector2(500, 350), true, 1);
@@ -17,3 +21,9 @@ func _on_Transition() -> void:
 
 func transition_end(anim_name : StringName) -> void:
 	get_tree().change_scene_to_packed(ResourceLoader.load_threaded_get(Globals.game_file));
+
+func _on_OpenCredits() -> void:
+	Credits.visible = true;
+
+func _on_CloseCredits() -> void:
+	Credits.visible = false;
