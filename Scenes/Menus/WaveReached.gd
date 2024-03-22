a744ac760f53aa6a67c9a5bd2c77b4670bd820ac
Simplexeon <gamerguy941mc@gmail.com>
2024-03-21 23:56:30 -0400

    Mousemode bugfix

--- Scenes/Menus/WaveReached.gd
+++ Scenes/Menus/WaveReached.gd
@@ -4,6 +4,7 @@ extends RichTextLabel
 # Processes
 
 func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE;
 	text = "[center]%d[/center]" % Globals.wave_reached;
 	bbcode_enabled = true;
 	ResourceLoader.load_threaded_request(Globals.game_file);
